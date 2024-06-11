82deb902f3a16d57ae0eda52e81ec168c6950d9d
unknown <mauriscode@gmail.com>
2024-06-11 12:56:52 -0300

    test3

--- pina_project/settings.py
+++ pina_project/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+#STATIC_URL = 'static/'
+#STATIC_ROOT = 'static/'
+STATIC_ROOT= '../pina/static/'
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
