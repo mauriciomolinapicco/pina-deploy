843a50983e289b9b5ea3e9ad8592e9eda0a5550f
unknown <mauriscode@gmail.com>
2024-06-13 09:26:55 -0300

    prova

--- pina_project/settings.py
+++ pina_project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 #STATIC_ROOT = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 # Default primary key field type
